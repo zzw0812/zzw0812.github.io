08b541908886239d19702e65c7f362b376578999
zzw0812 <57930461+zzw0812@users.noreply.github.com>
2020-07-29 15:58:03 +0800

    Create custom_layer_UNTMOD.py

---
+++ custom_layer_UNTMOD.py
@@ -0,0 +1,52 @@
+import os
+import random
+import numpy as np
+from keras.models import Model, load_model
+from keras.layers import Input, BatchNormalization, Activation, Dense, Dropout, ZeroPadding2D, AveragePooling2D, GlobalAveragePooling2D, multiply
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, GlobalMaxPool2D
+from keras.layers.merge import concatenate, add
+from keras.engine.topology import Layer
+from keras.engine import InputSpec
+from keras.layers import merge
+from keras import layers
+import keras.backend as K
+import tensorflow as tf
+from keras.layers import *
+
+
+class BilinearUpSampling2D(Layer):
+    """Upsampling2D with bilinear interpolation."""
+
+    def __init__(self, target_shape=None, factor=None, data_format=None, **kwargs):
+        if data_format is None:
+            data_format = K.image_data_format()
+        assert data_format in {
+            'channels_last', 'channels_first'}
+        self.data_format = data_format
+        self.input_spec = [InputSpec(ndim=4)]
+        self.target_shape = target_shape
+        self.factor = factor
+        if self.data_format == 'channels_first':
+            self.target_size = (target_shape[2], target_shape[3])
+        elif self.data_format == 'channels_last':
+            self.target_size = (target_shape[1], target_shape[2])
+        super(BilinearUpSampling2D, self).__init__(**kwargs)
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_last':
+            return (input_shape[0], self.target_size[0],
+                    self.target_size[1], input_shape[3])
+        else:
+            return (input_shape[0], input_shape[1],
+                    self.target_size[0], self.target_size[1])
+
+    def call(self, inputs):
+        return K.resize_images(inputs, self.factor, self.factor, self.data_format)
+
+    def get_config(self):
+        config = {'target_shape': self.target_shape,
+                'data_format': self.data_format}
+        base_config = super(BilinearUpSampling2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
